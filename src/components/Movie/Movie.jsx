6e08401d67f02cc7933e8d5341cf9bcf4e243ada
jferreira40 <ferreira.jess40@gmail.com>
2019-12-12 18:04:39 +0100

    Change class

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -9,10 +9,10 @@ export default class Movie extends Component {
 
     render() {
         return (
-            <div className="column is-m-3 card">
+            <div className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd is-m-3 card" id={this.props.id}>
                 <div className="card-image">
                     <figure className="image is-4by3">
-                        <img src={this.props.poster_path} alt={this.props.title}/>
+                        <img src={`https://image.tmdb.org/t/p/w200/${this.props.poster_path}`} alt={this.props.title}/>
                     </figure>
                 </div>
                 <div className="card-content">
